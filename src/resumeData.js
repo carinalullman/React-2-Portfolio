623ec5ca8b9c6406805c028af6e083b3f4c540a7
Carina Ullman <carinalullman@gmail.com>
2020-02-27 20:43:48 -0700

    Pushing Files Around

---
+++ src/resumeData.js
@@ -0,0 +1,125 @@
+let resumeData = {
+    //"imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Carina Ullman",
+    "role": "Full Stack Web Developer / Nurse / Military Veteran",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "  I will be graduating from DU's Full Stack Bootcamp in March 2020 and I am excited to cross over into the tech world.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/carina-ullman-0079782/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/carinalullman",
+          "className":"fa fa-github"
+        },
+        // {
+        //   "name":"skype",
+        //   "url":"http://twitter.com/rbhatia46",
+        //   "className":"fa fa-twitter"
+        // }
+      ],
+    "aboutme":"I am an Operating Room Nurse and Military Veteran.  I am currently a DU bootcamp student studying Full Stack Web Development.",
+    //"address":"India",
+    //"website":"https://rbhatia46.github.io",
+    "education":[
+      {
+        "UniversityName":"University of Denver",
+        "specialization":"Full Stack Web Development Certification",
+        "MonthOfPassing":"March",
+        "YearOfPassing":"2020",
+       
+      },
+      {
+        "UniversityName":"University of Colorado",
+        "specialization":"Bachelors in Nursing, BSN",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2016",
+        
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Parker Adventist Hospital",
+        "specialization":"Operating Room",
+        "MonthOfLeaving":"Currently Employed",
+        // "YearOfLeaving":"2018",
+        // "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Kronos, Inc",
+        "specialization":"Sales Support Specialist",
+        // "MonthOfLeaving":"Jan",
+        // "YearOfLeaving":"2018",
+        // "Achievements":"Some Achievements"
+        
+      },
+      {
+        "CompanyName":"United States Air Force",
+        "specialization":"Health Services Management",
+        // "MonthOfLeaving":"Jan",
+        // "YearOfLeaving":"2018",
+        // "Achievements":"Some Achievements"
+        
+      }
+    ],
+    "skillsDescription":"",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project5",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project6",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    // "testimonials":[
+    //   {
+    //     "description":"This is a sample testimonial",
+    //     "name":"Some technical guy"
+    //   },
+    //   {
+    //     "description":"This is a sample testimonial",
+    //     "name":"Some technical guy"
+    //   }
+    // ]
+  }
+  
+  export default resumeData
